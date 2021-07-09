fd854b81f588a374d7571d5dd1ff6f94cc490c58
Tej Singh <singhtejinder@google.com>
2021-07-09 00:37:06 -0700

    Slice conditions default to false
    Change the default value of sliced conditions from unknown to false.
    This is to save memory and avoid hitting the guardrail for sliced
    conditions with high cardinality, where we will continue to store keys
    after they become false.
    
    Test: atest statsd_test. added new tests & modified existing ones
    Bug: 191825214
    Change-Id: I7abf0bf04162cf68a26518117b61fcf96ecddde9

--- statsd/tests/metrics/parsing_utils/config_update_utils_test.cpp
+++ statsd/tests/metrics/parsing_utils/config_update_utils_test.cpp
@@ -2613,7 +2613,7 @@ TEST_F(ConfigUpdateTest, TestUpdateDurationMetrics) {
                                                   conditionCache));
     }
     // Predicate5 should be true since 2 uids have wakelocks
-    EXPECT_EQ(conditionCache, vector({kTrue, kUnknown, kUnknown, kUnknown, kUnknown}));
+    EXPECT_EQ(conditionCache, vector({kTrue, kFalse, kUnknown, kUnknown, kUnknown}));
 
     StatsdConfig newConfig;
     *newConfig.add_duration_metric() = duration6;
@@ -2716,7 +2716,7 @@ TEST_F(ConfigUpdateTest, TestUpdateDurationMetrics) {
             static_cast<DurationMetricProducer*>(newMetricProducers[duration1Index].get());
     EXPECT_EQ(durationProducer1->getMetricId(), duration1Id);
     EXPECT_EQ(durationProducer1->mConditionTrackerIndex, predicate4Index);
-    EXPECT_EQ(durationProducer1->mCondition, ConditionState::kUnknown);
+    EXPECT_EQ(durationProducer1->mCondition, ConditionState::kFalse);
     EXPECT_EQ(durationProducer1->mStartIndex, matcher3Index);
     EXPECT_EQ(durationProducer1->mStopIndex, matcher4Index);
     EXPECT_EQ(durationProducer1->mStopAllIndex, matcher7Index);
